d82bde55ba5b5549e89cf6266d1f0471c5f1e593
Renato Zanella Redoglia <renatozr07@gmail.com>
2024-09-09 14:10:13 +0000

    add: "Make a function that does arithmetic!" kata

---
+++ Python/7 kyu/Make a function that does arithmetic!.py
@@ -0,0 +1,9 @@
+def arithmetic(a, b, operator):
+    operate = {
+        "add": lambda a, b: a + b,
+        "subtract": lambda a, b: a - b,
+        "multiply": lambda a, b: a * b,
+        "divide": lambda a, b: a / b,
+    }
+
+    return operate[operator](a, b)
