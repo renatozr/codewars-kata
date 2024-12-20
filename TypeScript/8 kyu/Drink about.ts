2dbea224a5f990cf59d0b681d980ceb1ca6b2c36
Renato Zanella Redoglia <renatozr07@gmail.com>
2024-12-20 01:01:58 +0000

    add: "Drink about" kata

---
+++ TypeScript/8 kyu/Drink about.ts
@@ -0,0 +1,9 @@
+export function peopleWithAgeDrink(old: number): string {
+  return old < 14
+    ? 'drink toddy'
+    : old < 18
+    ? 'drink coke'
+    : old < 21
+    ? 'drink beer'
+    : 'drink whisky'
+}
