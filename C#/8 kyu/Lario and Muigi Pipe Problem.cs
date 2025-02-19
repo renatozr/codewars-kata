13966c9e35ccca2cd6770d072004adcda8283150
Renato Zanella Redoglia <renatozr07@gmail.com>
2025-02-19 02:29:01 +0000

    add: "Lario and Muigi Pipe Problem" kata

---
+++ C#/8 kyu/Lario and Muigi Pipe Problem.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public class Fixer
+{
+  public static List<int> PipeFix(List<int> numbers)
+  {
+    return Enumerable.Range(numbers.First(), numbers.Last() - numbers.First() + 1).ToList();
+  }
+}
